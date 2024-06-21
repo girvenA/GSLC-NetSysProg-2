7e2434bcb8dd369635dbdb197b76b2a0b6bc3b27
girvenA <girven.adrianus@binus.ac.id>
2024-06-21 20:53:25 +0700

    updated but not really updated

--- client.go
+++ client.go
@@ -4,35 +4,65 @@ import (
 	"bytes"
 	"encoding/json"
 	"fmt"
-	"io/ioutil"
+	"io"
 	"log"
 	"net/http"
 	"time"
 )
 
 func main() {
+	menu()
+}
+
+func menu() {
 	client := http.Client{
 		Timeout: 10 * time.Second,
 		Transport: &http.Transport{
 			DisableKeepAlives: true,
 		},
 	}
-	
+
 	url := "http://localhost:8080/testing"
 
+	var choose int = 0
+	for {
+		fmt.Println("Session 8")
+		fmt.Println("1. Receive data (get method)")
+		fmt.Println("2. Send data (post method)")
+		fmt.Println(">> ")
+		fmt.Scanf("%d", &choose)
+		switch choose {
+		case 1:
+			receiveMsg(client, url)
+		case 2:
+			sendMsg(client, url)
+		}
+	}
+}
+
+func msgExtractor(r *http.Response) (string, error) {
+	data, err := io.ReadAll(r.Body)
+	if err != nil {
+		return "", err
+	}
+	return string(data), nil
+
+}
+
+func receiveMsg(client http.Client, url string) {
 	resp, err := client.Get(url)
 	if err != nil {
 		log.Fatal(err)
 	}
-	defer resp.Body.Close()
-
-	body, err := ioutil.ReadAll(resp.Body)
+	msg, err := msgExtractor(resp)
 	if err != nil {
 		log.Fatal(err)
 	}
+	fmt.Println(msg)
+	return
+}
 
-	fmt.Println(string(body))
-
+func sendMsg(client http.Client, url string) {
 	data := map[string]interface{}{
 		"massage": "recived",
 	}
@@ -44,14 +74,14 @@ func main() {
 
 	buff := bytes.NewBuffer(jsonData)
 
-	resp, err = http.Post(url, "testing/jason", buff)
+	resp, err := http.Post(url, "testing/jason", buff)
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	defer resp.Body.Close()
-	
-	body, err = ioutil.ReadAll(resp.Body)
+
+	body, err := io.ReadAll(resp.Body)
 	if err != nil {
 		log.Fatal(err)
 	}
